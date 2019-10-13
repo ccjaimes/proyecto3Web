737bd4729adc5d96b8d3b5d9df2bc1bffc6347e3
cristhian <cc.pena@uniandes.edu.co>
2019-10-13 15:44:38 -0500

    cambios history

--- imports/ui/principal.js
+++ imports/ui/principal.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
-import createBrowserHistory from 'history/createBrowserHistory';
 
 //COMPONENTES
 import Inicio from "./inicio";
@@ -8,6 +7,7 @@ import Tendencia from "./tendencia";
 
 class Principal extends Component {
     render() {
+        
         return (
             <Router>
                 <div className="container-fluid">
@@ -33,25 +33,32 @@ class Principal extends Component {
                         </div>
                         <div className="col-10 col-md-4">
                             <div className="row">
-                                <div className="col-4 text-center" style={{ filter: "invert(100%)" }}>
+                                <div className="col-3 text-center" style={{ filter: "invert(100%)" }}>
                                     <Link to="/tendencia">
                                         <img src="https://image.flaticon.com/icons/svg/25/25652.svg" style={{ width: "30%" }} alt="Tendencia" />
                                         <br></br>
                                         Tendencia
                                     </Link>
                                 </div>
-                                <div className="col-4 text-center" style={{ filter: "invert(100%)" }}>
+                                <div className="col-3 text-center" style={{ filter: "invert(100%)" }}>
                                     <Link to="/buscar">
                                         <img src="https://image.flaticon.com/icons/svg/25/25313.svg" style={{ width: "30%" }} alt="Buscar" />
                                         <br></br>
                                         Buscar
                                     </Link>
                                 </div>
-                                <div className="col-4 text-center" style={{ filter: "invert(100%)" }}>
-                                    <Link>
+                                <div className="col-3 text-center" style={{ filter: "invert(100%)" }}>
+                                    <Link to="/recomendado">
                                         <img src="https://image.flaticon.com/icons/svg/149/149763.svg" style={{ width: "30%" }} alt="Recomendado" />
                                         <br></br>
                                         Recomendado
+                                    </Link>
+                                </div>
+                                <div className="col-3 text-center" style={{ filter: "invert(100%)" }}>
+                                    <Link to="/begin">
+                                        <img src="https://image.flaticon.com/icons/svg/74/74472.svg" style={{ width: "30%" }} alt="Recomendado"/>
+                                        <br></br>
+                                        Registrar
                                     </Link>
                                 </div>
                             </div>
