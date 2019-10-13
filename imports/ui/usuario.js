159ef75beb37942d56419bb9483d71e094b795c9
SEBASTIAN RODRIGUEZ BELTRAN <s.rodriguezb1@uniandes.edu.co>
2019-10-13 11:15:50 -0500

    Usuario basico

---
+++ imports/ui/usuario.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import { withTracker } from 'meteor/react-meteor-data';
+
+import { Usuario } from '../api/usuario.js';
+ 
+class Usuario extends Component {
+
+  render() {
+    return (
+      <div>
+          
+      </div>
+    );
+  }
+}
+
+export default Usuario;
--- server/main.js
+++ server/main.js
@@ -1,4 +1,7 @@
 import { Meteor } from 'meteor/meteor';
+import {Usuario} from '../imports/api/usuario.js';
+import {descuentoBD} from '../imports/api/descuentoBD.js';
+
 
 Meteor.startup(() => {
   // code to run on server at startup
