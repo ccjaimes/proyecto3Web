76d79a896098b6f6a390a30e2bc421c8ac807a19
cristian <cc.pena@uniandes.edu.co>
2019-10-14 17:55:02 -0500

    correccion error inicio

--- imports/ui/inicio.js
+++ imports/ui/inicio.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Inicio extends Component {
     render() {
         return (
-            <div class="container-fluid" style={{minHeight="100vh"}}>
+            <div class="container-fluid" style={{minHeight:"100vh"}}>
                 <div className="row">
                     <div className="col-12 col-md-6">
                         <img className="rounded-circle img-fluid mx-auto my-auto d-block" src="https://raw.githubusercontent.com/ccjaimes/proyecto3Web/master/img/discount.gif" />
