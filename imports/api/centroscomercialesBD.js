7b2526cff127cc7ed010581b374fa8f6857d0d98
lala341 <li.forero@uniandes.edu.co>
2019-10-14 09:36:43 -0500

    Cambios

--- imports/api/centroscomercialesBD.js
+++ imports/api/centroscomercialesBD.js
@@ -1,3 +1,3 @@
 import { Mongo } from 'meteor/mongo';
 
-export const CentrosComercialesBD = new Mongo.Collection('centroscomerciales');
+export const CentrosComercialesBD = new Mongo.Collection('centroComercial');
--- imports/ui/centroscomerciales.js
+++ imports/ui/centroscomerciales.js
@@ -11,7 +11,20 @@ class CentrosComerciales extends Component {
       <CentroComercialCard key={d._id} value={d} />
     ));
   }
+addElements(){
 
+  console.log("voy");
+  let dummyProfiles = [
+    { nombre: "Centro Comercial Calima", ubicacion: "Calle 1 f Bis No. 19-76, Bogotá D.C., Colombia", imagen:"https://media-cdn.tripadvisor.com/media/photo-s/0a/aa/cb/c5/centro-comercial-calima.jpg"},
+    { nombre: "Centro Comercial Centro mayor", ubicacion: "Calle 1 f Bis No. 19-76, Bogotá D.C., Colombia", imagen:"https://multiplaza-samantha.s3.amazonaws.com/uploads/custom_seo/image/5/Bogota.jpg" },
+    { nombre: "Centro Comercial Titan plaza", ubicacion: "Calle 1 f Bis No. 19-76, Bogotá D.C., Colombia", imagen:"https://bogota.gov.co/sites/default/files/styles/despliegue_1366x768_px/public/field/image/Centro-Comercial.gif" },
+    { nombre: "Centro Comercial Gran estación", ubicacion: "Calle 1 f Bis No. 19-76, Bogotá D.C., Colombia", imagen:"https://cr00.epimg.net/radio/imagenes/2015/02/14/bogota/1423915860_632758_1423940460_noticia_normal.jpg" },
+  ];
+
+  dummyProfiles.forEach(e => {
+    CentrosComercialesBD.insert(e);
+  });
+}
   render() {
     return (
       <div>
@@ -23,7 +36,7 @@ class CentrosComerciales extends Component {
         <div class="scro">
        
           {this.renderCentroComercial()}
-          
+
           </div>
       </div>
     );
@@ -31,6 +44,10 @@ class CentrosComerciales extends Component {
 }
 
 export default withTracker(() => {
+  
+  if(CentrosComercialesBD.find({}).fetch().length==0){
+    
+  }
   return {
     centrosComerciales: CentrosComercialesBD.find({}).fetch(),
   };
