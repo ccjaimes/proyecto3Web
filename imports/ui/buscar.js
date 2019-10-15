790d28ad42cf5fd598e777263958a34568bbab6f
cristian <cc.pena@uniandes.edu.co>
2019-10-14 21:59:12 -0500

    precambios

--- imports/ui/buscar.js
+++ imports/ui/buscar.js
@@ -4,67 +4,64 @@ import { withTracker } from 'meteor/react-meteor-data';
 
 import LocalCard from './localcard';
 import CentroComercialCard from './centroComercial/centrocomercialcard';
-import CentrosComercialesBD  from '../api/centroscomercialesBD';
+import CentrosComercialesBD from '../api/centroscomercialesBD';
 import LocalBD from '../api/localBD';
 
 class Buscar extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        console.log(this.props.locales)
-    this.state= {
-       
-        centros:[],
-        search:'',
-    }
+        this.state = {
 
-   // this.componentDidMount = this.componentDidMount.bind(this);
-    
-}
+            centros: [],
+            search: '',
+        }
+
+        // this.componentDidMount = this.componentDidMount.bind(this);
+
+    }
 
 
     updateSearch(event) {
-        this.setState ({search:event.target.value.substr(0,20)});
+        this.setState({ search: event.target.value.substr(0, 20) });
     }
 
-    
-     
+
+
     filterList = (event) => {
-            let items = this.state.centrosComerciales;
-            items = items.filter((item) => {
-              return item.nombre.toLowerCase().search(event.target.value.toLowerCase()) !== -1;
-            });
-            this.setState({centros: items})
+        let items = this.state.centrosComerciales;
+        items = items.filter((item) => {
+            return item.nombre.toLowerCase().search(event.target.value.toLowerCase()) !== -1;
+        });
+        this.setState({ centros: items })
     }
-          
+
 
     render() {
 
         let filtrados = this.props.centrosComerciales.filter(
-            (centro) =>{
-                return centro.nombre.toLowerCase().indexOf(this.state.search.toLowerCase()) !=-1;
-        }
-        );
-        
-        let locales = this.props.locales.filter(
-            (local) =>{
-                return local.Nombre.toLowerCase().indexOf(this.state.search.toLowerCase()) !=-1;
-        }
+            (centro) => {
+                return centro.nombre.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1;
+            }
         );
 
+        let locales = this.props.locales.filter(
+            (local) => {
+                return local.Nombre.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1;
+            }
+        );
 
         return (
             <div>
                 <div className="md-form mt-0">
-                <input className="form-control" type="text" placeholder="Search" aria-label="Search" value={this.state.search} onChange={(this.updateSearch.bind(this))}/>
-                 </div>
-                  <hr></hr>
+                    <input className="form-control" type="text" placeholder="Search" aria-label="Search" value={this.state.search} onChange={(this.updateSearch.bind(this))} />
+                </div>
+                <hr></hr>
                 <h2>Centros Comerciales</h2>
                 <br></br>
                 <ul>
-                {filtrados.map((centro)=>
-                {
-                    <CentroComercialCard key={centro._id} value={centro} />
-                })}
+                    {filtrados.map((centro) => {
+                        <CentroComercialCard key={centro._id} value={centro} />
+                    })}
                 </ul>
                 <br></br>
                 <hr></hr>
@@ -72,21 +69,23 @@ class Buscar extends Component {
                 <h2>Locales</h2>
                 <br></br>
                 <ul>
-                {locales.map((local)=>
-                {
-                    <LocalCard key={local._id} value={local} />
-                })}
+                    {locales.map((local) => {
+                        <LocalCard key={local._id} value={local} />
+                    })}
                 </ul>
 
-              
+
             </div>
         );
     }
 }
 
 export default withTracker(() => {
+    
+    console.log(LocalBD.find({}).fetch());
+    console.log(CentrosComercialesBD.find({}).fetch());
     return {
-      centrosComerciales: CentrosComercialesBD.find({}).fetch(),
-      locales: LocalBD.find({}).fetch()
+        centrosComerciales: CentrosComercialesBD.find({}).fetch(),
+        locales: LocalBD.find({}).fetch()
     };
-  })(Buscar);
+})(Buscar);
