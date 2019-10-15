0b07625c71173af5f3e43fc964d88d948e697322
SEBASTIAN RODRIGUEZ BELTRAN <s.rodriguezb1@uniandes.edu.co>
2019-10-14 21:37:10 -0500

    Y buscar sigue y sigue

--- imports/ui/buscar.js
+++ imports/ui/buscar.js
@@ -1,20 +1,24 @@
 import React, { Component } from 'react';
+
+import { withTracker } from 'meteor/react-meteor-data';
+
 import LocalCard from './localcard';
 import CentroComercialCard from './centroComercial/centrocomercialcard';
 import CentrosComercialesBD  from '../api/centroscomercialesBD';
 import LocalBD from '../api/localBD';
 
 class Buscar extends Component {
-    constructor(){
-        super();
+    constructor(props){
+        super(props);
+        console.log(this.props.locales)
     this.state= {
-        centrosComerciales:CentrosComercialesBD.find({}).fetch(),
-        locales:LocalBD.find({}).fetch(),
+       
         centros:[],
         search:'',
     }
 
-    this.componentDidMount = this.componentDidMount.bind(this);
+   // this.componentDidMount = this.componentDidMount.bind(this);
+    
 }
 
 
@@ -22,22 +26,26 @@ class Buscar extends Component {
         this.setState ({search:event.target.value.substr(0,20)});
     }
 
-    componentDidMount = () => {
-        this.setState({
-            centrosComerciales: CentrosComercialesBD.find({}).fetch(),
-            locales: LocalBD.find({}).fetch() 
-        })}
+    
      
+    filterList = (event) => {
+            let items = this.state.centrosComerciales;
+            items = items.filter((item) => {
+              return item.nombre.toLowerCase().search(event.target.value.toLowerCase()) !== -1;
+            });
+            this.setState({centros: items})
+    }
+          
 
     render() {
 
-        let filtrados = this.state.centrosComerciales.filter(
+        let filtrados = this.props.centrosComerciales.filter(
             (centro) =>{
                 return centro.nombre.toLowerCase().indexOf(this.state.search.toLowerCase()) !=-1;
         }
         );
         
-        let locales = this.state.locales.filter(
+        let locales = this.props.locales.filter(
             (local) =>{
                 return local.Nombre.toLowerCase().indexOf(this.state.search.toLowerCase()) !=-1;
         }
@@ -47,7 +55,7 @@ class Buscar extends Component {
         return (
             <div>
                 <div className="md-form mt-0">
-                <input className="form-control" type="text" placeholder="Search" aria-label="Search" value={this.state.search} onChange={this.updateSearch.bind(this)}/>
+                <input className="form-control" type="text" placeholder="Search" aria-label="Search" value={this.state.search} onChange={(this.updateSearch.bind(this))}/>
                  </div>
                   <hr></hr>
                 <h2>Centros Comerciales</h2>
@@ -76,4 +84,9 @@ class Buscar extends Component {
     }
 }
 
-export default Buscar;
+export default withTracker(() => {
+    return {
+      centrosComerciales: CentrosComercialesBD.find({}).fetch(),
+      locales: LocalBD.find({}).fetch()
+    };
+  })(Buscar);
